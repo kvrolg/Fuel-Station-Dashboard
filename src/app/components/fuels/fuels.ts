ea658de502b0f45a3a4ba83cf89ea68d6a841f48
kvrolg <karolgrzyb09@gmail.com>
2026-05-07 11:01:07 +0200

    Removed NgForImport

--- src/app/components/fuels/fuels.ts
+++ src/app/components/fuels/fuels.ts
@@ -1,18 +1,18 @@
 import { Component, inject, OnInit} from '@angular/core';
 import { FuelService } from '../../fuel-service';
 import { FuelModel } from '../../models/fuel.model';
-import { NgForOf, NgClass, AsyncPipe } from "@angular/common";
+import { AsyncPipe } from "@angular/common";
 import { Observable } from 'rxjs';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-// import { DataService } from './data.service';
+import { DataService } from './data.service';
 import {MatButtonModule} from '@angular/material/button';
 
 
 @Component({
   selector: 'app-fuels',
-  imports: [NgForOf, NgClass, AsyncPipe, MatButtonModule, MatTableModule, MatFormFieldModule, MatInputModule],
+  imports: [AsyncPipe, MatButtonModule, MatTableModule, MatFormFieldModule, MatInputModule],
   templateUrl: './fuels.html',
   styleUrl: './fuels.scss',
 })
@@ -27,6 +27,6 @@ export class Fuels implements OnInit{
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
   ngOnInit(): void {
-    // this.fuels$.getData()
+    this.fuels$.getData()
   }
 }
