db951d5485f7a60f2aff6062269c43fafbc1a158
kvrolg <karolgrzyb09@gmail.com>
2026-05-07 10:53:33 +0200

    Micro styling changes

--- src/app/components/fuels/fuels.ts
+++ src/app/components/fuels/fuels.ts
@@ -1,20 +1,32 @@
-import { Component, inject} from '@angular/core';
+import { Component, inject, OnInit} from '@angular/core';
 import { FuelService } from '../../fuel-service';
 import { FuelModel } from '../../models/fuel.model';
 import { NgForOf, NgClass, AsyncPipe } from "@angular/common";
 import { Observable } from 'rxjs';
-import {MatTableModule} from '@angular/material/table';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+// import { DataService } from './data.service';
 import {MatButtonModule} from '@angular/material/button';
 
 
 @Component({
   selector: 'app-fuels',
-  imports: [NgForOf, NgClass, AsyncPipe, MatTableModule, MatButtonModule],
+  imports: [NgForOf, NgClass, AsyncPipe, MatButtonModule, MatTableModule, MatFormFieldModule, MatInputModule],
   templateUrl: './fuels.html',
   styleUrl: './fuels.scss',
 })
-export class Fuels{
+export class Fuels implements OnInit{
   private fuelsList = inject(FuelService);
   fuels$: Observable<FuelModel[]> = this.fuelsList.getFuels();
   keys: string[] = ['name', 'price', 'category', 'available', 'stockLevel', 'premium'];
+  dataSource = new MatTableDataSource(this.keys)
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  ngOnInit(): void {
+    // this.fuels$.getData()
+  }
 }
