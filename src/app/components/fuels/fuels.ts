e5fa209a72a6825afef41f3826220a93355f0dd0
kvrolg <karolgrzyb09@gmail.com>
2026-05-07 11:02:50 +0200

    Removed Ng import

--- src/app/components/fuels/fuels.ts
+++ src/app/components/fuels/fuels.ts
@@ -1,7 +1,7 @@
 import { Component, inject} from '@angular/core';
 import { FuelService } from '../../fuel-service';
 import { FuelModel } from '../../models/fuel.model';
-import { NgForOf, NgClass, AsyncPipe } from "@angular/common";
+import { AsyncPipe } from "@angular/common";
 import { Observable } from 'rxjs';
 import {MatTableModule} from '@angular/material/table';
 import {MatButtonModule} from '@angular/material/button';
@@ -9,7 +9,7 @@ import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-fuels',
-  imports: [NgForOf, NgClass, AsyncPipe, MatTableModule, MatButtonModule],
+  imports: [AsyncPipe, MatTableModule, MatButtonModule],
   templateUrl: './fuels.html',
   styleUrl: './fuels.scss',
 })
