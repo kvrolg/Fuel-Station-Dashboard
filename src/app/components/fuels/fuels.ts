2193fdaf5f5e49e467457ade82f93fff79d470ba
kvrolg <karolgrzyb09@gmail.com>
2026-05-07 16:30:56 +0200

    Trying to add filtering fuels table by name

--- src/app/components/fuels/fuels.ts
+++ src/app/components/fuels/fuels.ts
@@ -1,32 +1,46 @@
-import { Component, inject, OnInit} from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FuelService } from '../../fuel-service';
 import { FuelModel } from '../../models/fuel.model';
-import { AsyncPipe } from "@angular/common";
-import { Observable } from 'rxjs';
-import {MatTableDataSource, MatTableModule} from '@angular/material/table';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { DataService } from './data.service';
-import {MatButtonModule} from '@angular/material/button';
-
+import { AsyncPipe } from '@angular/common';
+import { Observable, tap } from 'rxjs';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule, MatProgressSpinner } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-fuels',
-  imports: [AsyncPipe, MatButtonModule, MatTableModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    AsyncPipe,
+    MatButtonModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatProgressSpinner,
+  ],
   templateUrl: './fuels.html',
   styleUrl: './fuels.scss',
 })
-export class Fuels implements OnInit{
+export class Fuels {
   private fuelsList = inject(FuelService);
-  fuels$: Observable<FuelModel[]> = this.fuelsList.getFuels();
+  fuels$: Observable<FuelModel[]> = this.fuelsList.getFuels().pipe(
+    tap((items) => {
+      this.dataSource.data = items;
+      this.dataSource.filterPredicate = (data: FuelModel, filter: string): boolean => { 
+        console.log(data)
+        return data.name.toLowerCase().includes(filter.trim().toLowerCase());
+      };
+    }),
+  );
   keys: string[] = ['name', 'price', 'category', 'available', 'stockLevel', 'premium'];
-  dataSource = new MatTableDataSource(this.keys)
+  dataSource = new MatTableDataSource<FuelModel>([]);
+  fuelTable: FuelModel[] = [];
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
+
     this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
-  ngOnInit(): void {
-    this.fuels$.getData()
+    // console.log(this.dataSource.data);
   }
 }
