b1eb3dc25148b863570976e49fcbe6b2bf8f5c83
kvrolg <karolgrzyb09@gmail.com>
2026-05-11 10:16:14 +0200

    Removed "::ng-deep", host duplication. Changed id declarations to class in css.

--- src/app/components/fuels/fuels.ts
+++ src/app/components/fuels/fuels.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { FuelService } from '../../fuel-service';
 import { FuelModel } from '../../models/fuel.model';
 import { AsyncPipe } from '@angular/common';
@@ -7,9 +7,9 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
-import { MatProgressSpinnerModule, MatProgressSpinner } from '@angular/material/progress-spinner';
 import { MatSlideToggleModule, MatSlideToggle } from '@angular/material/slide-toggle';
 import { FormsModule } from '@angular/forms';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-fuels',
@@ -27,6 +27,7 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './fuels.scss',
 })
 export class Fuels {
+  @ViewChild(MatSort) sort!: MatSort;
   private fuelsList = inject(FuelService);
   isAvailable: boolean = false;
   isPremium: boolean = false;
@@ -47,6 +48,7 @@ export class Fuels {
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+    console.log(this.dataSource.data);
   }
   onToggleChange(): void {
     const currentFilter = this.dataSource.filter;
