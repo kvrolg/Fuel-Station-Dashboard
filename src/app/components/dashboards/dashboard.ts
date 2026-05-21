d427f98b371e0da4a21b84ba78b630e85c2b18d6
kvrolg <karolgrzyb09@gmail.com>
2026-05-21 10:58:08 +0200

    Css and html optymalization in dashboard.

--- src/app/components/dashboards/dashboard.ts
+++ src/app/components/dashboards/dashboard.ts
@@ -1,25 +1,33 @@
-import { Component, inject } from '@angular/core';
-import { Observable, tap } from 'rxjs';
-import { Station } from '../../models/station.model';
-import { MatTableDataSource } from '@angular/material/table';
-import {MatCardModule} from '@angular/material/card';
 import { AsyncPipe, DecimalPipe } from '@angular/common';
-import { Fuels } from "../fuels/fuels";
-import { Promotions } from "../promotions/promotions";
+import { Component, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Observable, tap } from 'rxjs';
+import { DashboardService } from '../../dashboard-service';
 import { FuelService } from '../../fuel-service';
 import { FuelModel } from '../../models/fuel.model';
-import { MatIcon } from "@angular/material/icon";
-import { DashboardService } from '../../dashboard-service';
-import { PromotionService } from '../../promotion-service';
 import { Promotion } from '../../models/promotion.model';
-import { StationServicesService } from '../../station-services-service';
+import { Station } from '../../models/station.model';
 import { StationService } from '../../models/stationService.model';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatTableModule} from '@angular/material/table';
+import { PromotionService } from '../../promotion-service';
+import { StationServicesService } from '../../station-services-service';
+import { Fuels } from '../fuels/fuels';
+import { Promotions } from '../promotions/promotions';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [MatCardModule, AsyncPipe, Fuels, Promotions, MatIcon, MatProgressBarModule, DecimalPipe, MatTableModule],
+  imports: [
+    MatCardModule,
+    AsyncPipe,
+    Fuels,
+    Promotions,
+    MatIcon,
+    MatProgressBarModule,
+    DecimalPipe,
+    MatTableModule,
+  ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
@@ -34,11 +42,16 @@ export class Dashboard {
   servicesData = new MatTableDataSource<StationService>();
   fuelsDataColumns: string[] = ['type', 'price', 'stock'];
 
-  station$: Observable<Station[]> = this.dashboardService.getStation().pipe(tap((items) => this.dataSource.data = items))
-  fuels$: Observable<FuelModel[]> = this.fuelsService.getAvailableFuels().pipe(tap((items) => this.fuelsData.data = items))
-  promotions$: Observable<Promotion[]> = this.promotionsService.getActivePromotions().pipe(tap((items) => this.promotionsData.data = items))
-  services$: Observable<StationService[]> = this.servicesService.getAvailableServices().pipe(tap((items) => this.servicesData.data = items))
-
-  
-
+  station$: Observable<Station[]> = this.dashboardService
+    .getStation()
+    .pipe(tap((items) => (this.dataSource.data = items)));
+  fuels$: Observable<FuelModel[]> = this.fuelsService
+    .getAvailableFuels()
+    .pipe(tap((items) => (this.fuelsData.data = items)));
+  promotions$: Observable<Promotion[]> = this.promotionsService
+    .getActivePromotions()
+    .pipe(tap((items) => (this.promotionsData.data = items)));
+  services$: Observable<StationService[]> = this.servicesService
+    .getAvailableServices()
+    .pipe(tap((items) => (this.servicesData.data = items)));
 }
