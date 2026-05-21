b0dae3caa2e37a9aa6ce23e9f04ed6f861153a07
kvrolg <karolgrzyb09@gmail.com>
2026-05-21 13:06:17 +0200

    Added spinner showing when no data

--- src/app/components/fuels/fuels.ts
+++ src/app/components/fuels/fuels.ts
@@ -17,6 +17,7 @@ import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
 import { ChangePriceComponent } from './change-price-component/change-price-component';
 import { MatDialog } from '@angular/material/dialog';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { MatProgressSpinner } from "@angular/material/progress-spinner";
 
 interface FilterState {
   name: string;
@@ -35,9 +36,9 @@ interface FilterState {
     MatSlideToggle,
     MatSlideToggleModule,
     FormsModule,
-    MatSortModule
-    
-  ],
+    MatSortModule,
+    MatProgressSpinner
+],
   templateUrl: './fuels.html',
   styleUrl: './fuels.scss',
 })
